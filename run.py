3fbac1ded6052f2b1d9ce629e8e3b93cdbcaef42
narain <narain13579@gmail.com>
2021-05-15 21:22:47 +0530

    add progress bar

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from sklearn.model_selection import KFold
 from tqdm.auto import tqdm
 
 for file in tqdm(os.listdir(os.getcwd())):
-    if file.endswith('.ipynb'): subprocess.run("jupyter nbconvert --to notebook --execute {file} --output
-                                               {file}".split())
+    if file.endswith('.ipynb'):
+        subprocess.run("jupyter nbconvert --to notebook --execute {file} --output {file}".split())
