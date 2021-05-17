7575095685e476822c9488a094ba8b38ef112d44
narain <narain13579@gmail.com>
2021-05-17 17:29:06 +0530

    holy push

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ import subprocess
 from sklearn.model_selection import KFold
 from tqdm.auto import tqdm
 
-if os.makedirs(['exp'], exist_ok=True)
+os.makedirs(['exp'], exist_ok=True)
+
 for file in tqdm(os.listdir(os.getcwd())):
     if file.endswith('.ipynb'):
         subprocess.run(f"jupyter nbconvert --to notebook --execute {file} --output {file} --output-dir nbs".split())
