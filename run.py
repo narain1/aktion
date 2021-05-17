27597b68b92b566def10d1bfef1b0dfc96dca5cb
narain <narain13579@gmail.com>
2021-05-17 17:23:26 +0530

    run files on sep directory

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import subprocess
 from sklearn.model_selection import KFold
 from tqdm.auto import tqdm
 
+if os.makedirs(['exp'], exist_ok=True)
 for file in tqdm(os.listdir(os.getcwd())):
     if file.endswith('.ipynb'):
-        subprocess.run(f"jupyter nbconvert --to notebook --execute {file} --output {file}".split())
+        subprocess.run(f"jupyter nbconvert --to notebook --execute {file} --output {file} --output-dir nbs".split())
